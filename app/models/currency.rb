bb0c3b3a4d1a60e0026618fa4fa509b33933a236
Olesya Miller <olesyam.miller@gmail.com>
2021-03-28 22:13:28 -0400

    created current_price method

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,8 @@
 class Currency < ApplicationRecord
+
+    def current_price
+        url = 'https://api.coingecko.com/api/v3/coins/'
+        request = HTTParty.get(url + self.slug)
+        response = JSON.parse(request.body)["market_data"]["current_price"]["usd"]
+    end
 end
