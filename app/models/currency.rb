6964d1156b23cd61e6d22a7aed6c12d631a3b71f
Olesya Miller <olesyam.miller@gmail.com>
2021-03-28 22:24:49 -0400

    created calculate_value method

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,5 +1,9 @@
 class Currency < ApplicationRecord
 
+    def calculate_value(amount)
+        (current_price * amount.to_f).round(4)
+    end
+
     def current_price
         url = 'https://api.coingecko.com/api/v3/coins/'
         request = HTTParty.get(url + self.slug)
