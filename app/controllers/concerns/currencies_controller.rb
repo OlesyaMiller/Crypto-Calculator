d623d32d851deb42a3604d27cea6d9e6cad09aa5
Olesya Miller <olesyam.miller@gmail.com>
2021-03-29 13:08:21 -0400

    added logic to currencies controller

--- app/controllers/concerns/currencies_controller.rb
+++ app/controllers/concerns/currencies_controller.rb
@@ -1,3 +1,9 @@
 class CurrenciesController < ApplicationController
+    def index
+    end
 
+    def search
+        @currencies = Carrency.where('LOWER(name) LIKE ?', "%#{params[:search].downcase}%")
+        render json: {currencies: @currencies}
+    end
 end 
