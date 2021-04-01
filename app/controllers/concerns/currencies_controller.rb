d5927f0c967cc5bf1b5983e383295b03b4e6d351
Olesya Miller <olesyam.miller@gmail.com>
2021-04-01 11:35:07 -0400

    created calculate method

--- app/controllers/concerns/currencies_controller.rb
+++ app/controllers/concerns/currencies_controller.rb
@@ -6,4 +6,22 @@ class CurrenciesController < ApplicationController
         @currencies = Carrency.where('LOWER(name) LIKE ?', "%#{params[:search].downcase}%")
         render json: {currencies: @currencies}
     end
+    #takes in the currency id and the amount owned
+    #returns final calculations
+    def calculate 
+        amount = params[:amount]
+        render json: {
+            currency: currency,
+            current_price: currency.current_price,
+            amount: amount,
+            value: currency.calculate_value(amount)
+        }
+    end
+
+    private 
+
+    def currency 
+        @currency ||= Currency.find(params[:id])
+
+    end
 end 
