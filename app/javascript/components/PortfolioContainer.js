8b4831d8300ad3d10f472f14826b907fde9bda08
Olesya Miller <olesyam.miller@gmail.com>
2021-04-01 11:54:12 -0400

    created Search component

---
+++ app/javascript/components/PortfolioContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class PortfolioContainer extends Component {
+    state = {
+        name: '',
+        portfolio: [],
+        search_results: [],
+        active_currency: null,
+        amount: null
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PortfolioContainer;
---
+++ app/javascript/components/Search.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <div>
+                Search goes here 
+            </div>
+        );
+    }
+}
+
+export default Search;
