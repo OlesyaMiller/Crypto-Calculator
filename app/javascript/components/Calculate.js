19bdb90ece40a210e70580c05164a821f32cc516
Olesya Miller <olesyam.miller@gmail.com>
2021-04-01 11:55:20 -0400

    created Calculate component

---
+++ app/javascript/components/Calculate.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Calculate extends Component {
+    state = {}
+
+    render() {
+        return (
+            <div>
+                Calculate goes here 
+            </div>
+        );
+    }
+}
+
+export default Calculate;
