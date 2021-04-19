80c132f9ed3c561e8dcb52f8d650e2d877e3b65a
Olesya Miller <olesyam.miller@gmail.com>
2021-04-19 18:43:19 -0400

    refactoring

--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -16,7 +16,6 @@ class PortfolioContainer extends Component {
             search: e.target.value
         })
         .then((data) => {
-            // })
             debugger 
         })
         .catch((data) => {
