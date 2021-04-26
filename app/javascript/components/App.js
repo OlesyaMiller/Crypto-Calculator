a6b4101c45b8c115296e3ffd5ef9832060c29108
Olesya Miller <olesyam.miller@gmail.com>
2021-04-26 18:08:50 -0400

    test

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PortfolioContainer from './PortfolioContainer'
-import axios from 'axios';
+import axios from 'axios'
 
 const csrfToken = document.querySelector('[name="csrf-token"]').content
 axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken 
