b9765e76c2c7a067816236a6c81ac3585a45373b
olesya <olesyam.miller@gmail.com>
2022-06-03 00:47:06 -0400

    add calculate and private currency method

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -8,5 +8,18 @@ class CurrenciesController < ApplicationController
     end
 
     def calculate
+        amount = params[:amount]
+        render json: {
+            currency: currency,
+            current_price: currency.current_price,
+            amount: amount,
+            value: currency.calculate_value(amount)
+        }
+    end
+
+    private
+
+    def currency
+        @currency ||= Currency.find(params[:id])
     end
 end
