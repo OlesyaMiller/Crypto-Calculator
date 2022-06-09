72b023f361b3aeb51ede8acf601614981c50a797
olesya <olesyam.miller@gmail.com>
2022-06-08 21:19:42 -0400

    implement search functionality

--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Search from './Search'
 import Calculate from './Calculate'
+import axios from 'axios'
 
 class PortfolioContainer extends Component {
     state = {
@@ -12,17 +13,29 @@ class PortfolioContainer extends Component {
     }
 
     handleChange = (e) => {
-        debugger
-        this.setState({
-            [e.target.name]: e.target.value
+        // this.setState({
+        //     [e.target.name]: e.target.value
+        // })
+
+        axios.post('http://localhost:3000/search', {
+            search: e.target.value
         })
-        console.log(this.state.name)
+        .then((data) => {
+            this.setState({
+                search_results: [...data.data.currencies]            
+            })
+        })
+        .catch((data) => {
+            debugger
+        })
+
+        console.log(this.state.search_results)
     }
 
     render() {
         return (
             <div>
-                <Search handleChange={this.handleChange}/>
+                <Search searchResults={this.state.search_results} handleChange={this.handleChange}/>
                 <Calculate/>
             </div>
         )
--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Search extends Component {
     
     render() {
+        const searchResults = this.props.searchResults.map(curr => <li key={curr.id} className="currency-list-item"><a class="currency" href="#"><span>{curr.name}</span><span>{curr.currency_symbol}</span></a></li>)
         return (
             <div>
                 <h1>Cryptocurrency Portfolio Calculator</h1>
@@ -12,6 +13,9 @@ class Search extends Component {
                         <input onChange={this.props.handleChange} autoComplete="off" type="text" name="name" placeholder="Ex: Bitcoin, Ethereum" value={this.props.name} className="field"/>    
                     </div>    
                 </form> 
+                <div className="currency-list">
+                    {searchResults}
+                </div>
             </div>
         )
     }
